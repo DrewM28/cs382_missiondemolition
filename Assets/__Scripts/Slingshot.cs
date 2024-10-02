3cde1e4d868286e7c606e2071591e7d642375700
Drew Milton <dmgolf28@gmail.com>
2024-10-01 23:56:21 -0500

    adding new files

--- Assets/__Scripts/Slingshot.cs
+++ Assets/__Scripts/Slingshot.cs
@@ -76,10 +76,15 @@ public class Slingshot : MonoBehaviour
             projRB.isKinematic = false;
             projRB.collisionDetectionMode = CollisionDetectionMode.Continuous;
             projRB.velocity = -mouseDelta * velocityMult;
+
+            //Switch to slingshot view immediately before setting POI
+            FollowCam.SWITCH_VIEW( FollowCam.eView.slingshot );
+
             FollowCam.POI = projectile; //Set the _MainCamera POI
             //Add a projectile line to the projectile
             Instantiate<GameObject>(projLinePrefab, projectile.transform);
             projectile = null;
+            MissionDemolition.SHOT_FIRED();
         }
     }
 
